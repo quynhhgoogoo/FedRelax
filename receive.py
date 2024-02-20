3570ea03f690d6064003e38466f1e214fd4f7220
Quynh Luong <luongdiemquynh1998@gmail.com>
2024-02-20 23:29:38 +0200

    add server and client, some automation for query ip address inside pods, draft, still need to test more

--- receive.py
+++ receive.py
@@ -2,16 +2,39 @@ import socket
 import os
 import sys
 import time
+from kubernetes import client, config
 
 counter = 0
 
-SRV = "server_ip"
+def get_pod_ip():
+    # Get the hostname of the pod
+    hostname = socket.gethostname()
+    # Get the IP address corresponding to the hostname
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+def get_pod_info(namespace):
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+    pod_info = {}
+    pods = v1.list_namespaced_pod(namespace=namespace)
+    for pod in pods.items:
+        pod_info[pod.metadata.name] = pod.status.pod_ip
+    return pod_info
+
+pod_index = get_pod_ip()
+peer_pods = get_pod_info("fed-relax")
+
+for peer_pod_index, peer_pod_ip in peer_pods.items():
+    if peer_pod_ip != pod_index:
+        # Need to rewrite this. Will be different case if the number of pods are > 2
+        SRV = peer_pod_ip
+
 PORT = 3000
 
 while 1:
     if counter != 0:
         time.sleep(5)
-
     counter += 1
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (SRV, PORT)
@@ -22,7 +45,6 @@ while 1:
     except Exception as e:
         print("Cannot connect to the server,", e)
         continue
-
     try:
         message = b'This is the message. It will be repeated.'
         print('Sending:  {!r}'.format(message))
--- send.py
+++ send.py
@@ -1,11 +1,38 @@
 import socket
 import sys
 import os
+from kubernetes import client, config
 
 PORT = 3000
 
+def get_pod_ip():
+    # Get the hostname of the pod
+    hostname = socket.gethostname()
+    # Get the IP address corresponding to the hostname
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+def get_pod_info(namespace):
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+    pod_info = {}
+    pods = v1.list_namespaced_pod(namespace=namespace)
+    for pod in pods.items:
+        pod_info[pod.metadata.name] = pod.status.pod_ip
+    return pod_info
+
+pod_index = get_pod_ip()
+peer_pods = get_pod_info("fed-relax")
+
+for peer_pod_index, peer_pod_ip in peer_pods.items():
+    if peer_pod_ip != pod_index:
+        print(peer_pod_ip, pod_index)
+        # Need to rewrite this. Will be different case if the number of pods are > 2
+        SRV = peer_pod_ip
+
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('receiver_ip', PORT)
+server_address = (SRV, PORT)
 print('Starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 sock.listen()
