dfc0b45256dbcce7fc770841543cd96a5685ff06
Quynh Luong <luongdiemquynh1998@gmail.com>
2024-06-06 23:09:30 +0300

    dry run, mounting data from local device to pod

--- data_partition.py
+++ data_partition.py
@@ -2,15 +2,14 @@ import os
 import pickle
 import numpy as np
 
-def partition_graph_data(graph_file, output_dir, pod_name):
+def partition_graph_data(graph_file, output_dir):
     # Load the graph object from the pickle file
     with open(graph_file, 'rb') as f:
         G = pickle.load(f)
 
     # Ensure the output directory exists
-    pod_output_dir = os.path.join(output_dir, f'partition_{pod_name}')
-    if not os.path.exists(pod_output_dir):
-        os.makedirs(pod_output_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Iterate over each node in the graph and save its data to a separate file
     for node in G.nodes():
@@ -21,8 +20,8 @@ def partition_graph_data(graph_file, output_dir, pod_name):
         node_data['coords'] = nodefeatures
         node_data["name"] = str(node)
         
-        # Save the node data to a file within the pod's partition directory
-        node_file = os.path.join(pod_output_dir, f'node_{node}_data.pickle')
+        # Save the node data to a file
+        node_file = os.path.join(output_dir, f'node_{node}_data.pickle')
         with open(node_file, 'wb') as nf:
             pickle.dump(node_data, nf)
         
@@ -35,14 +34,8 @@ if __name__ == "__main__":
     # Construct the path to the pickle file relative to the current directory
     graph_data = os.path.join(current_dir, 'algorithm', 'data', 'QuizGraphLearning.pickle')
 
-    # Get the pod's name from the environment variable
-    pod_name = os.environ.get('POD_NAME', 'unknown_pod')
-
-    if pod_name == 'unknown_pod':
-        raise ValueError("POD_NAME environment variable not set")
-
     # Construct the path to the output directory
-    partition_dir = os.path.join(current_dir, 'data')
+    partition_dir = os.path.join(current_dir, 'algorithm', 'data', 'input-data-on-laptop')
 
     # Partition the graph data into separate files
-    partition_graph_data(graph_data, partition_dir, pod_name)
+    partition_graph_data(graph_data, partition_dir)
